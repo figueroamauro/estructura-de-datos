bdb5ccdd3a63e0b69d39622ebc525c86c978a5bd
mauro <figueroamauro.dev@gmail.com>
2025-02-04 00:46:45 -0300

    refactor(SingleLinkedListTest): add beforeEach anottation to simplify logic and add completeList() method to add multiple nodes to the list.

--- data-structure/src/test/java/ar/com/old/nodes/SingleLinkedListTest.java
+++ data-structure/src/test/java/ar/com/old/nodes/SingleLinkedListTest.java
@@ -3,13 +3,18 @@ package ar.com.old.nodes;
 import static org.junit.jupiter.api.Assertions.*;
 
 import ar.com.old.exceptions.EmptyLinkedListException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class SingleLinkedListTest {
+    SingleLinkedList list = new SingleLinkedList();
+    @BeforeEach
+    void init() {
+         list = new SingleLinkedList();
+    }
 
     @Test
     void shouldCreateSingleLinkedList() {
-        SingleLinkedList list = new SingleLinkedList();
         assertNotNull(list);
         assertNull(list.head);
         assertEquals(0, list.size);
@@ -17,7 +22,6 @@ public class SingleLinkedListTest {
 
     @Test
     void shouldAddNode() {
-        SingleLinkedList list = new SingleLinkedList();
         list.add(1);
         assertEquals(1, list.head.value);
         assertEquals(1, list.size);
@@ -25,30 +29,29 @@ public class SingleLinkedListTest {
 
     @Test
     void shouldRemoveNode() {
-        SingleLinkedList list = new SingleLinkedList();
-        list.add(1);
-        list.add(2);
-        list.add(3);
+        completeList();
         list.remove(2);
         assertEquals(2, list.size);
     }
 
     @Test
     void shouldThrowExceptionRemovingNode_withEmptyList(){
-    SingleLinkedList list = new SingleLinkedList();
     Exception exception =assertThrows(EmptyLinkedListException.class, () -> list.remove(1));
     assertEquals("La lista esta vacia", exception.getMessage());
     }
 
     @Test
     void shouldVerifyIfNodeExists() {
-        SingleLinkedList list = new SingleLinkedList();
-        list.add(1);
-        list.add(2);
-        list.add(3);
+        completeList();
         assertTrue(list.contains(1));
         assertTrue(list.contains(2));
         assertTrue(list.contains(3));
         assertFalse(list.contains(4));
+    }
+
+    private void completeList() {
+        list.add(1);
+        list.add(2);
+        list.add(3);
     }
 }
