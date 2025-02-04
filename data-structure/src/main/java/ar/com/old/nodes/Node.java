b9e7d22c2568c2c04d5dfbf60288308d356a0d53
mauro <figueroamauro.dev@gmail.com>
2025-02-03 22:48:03 -0300

    feat/test: add Node class and its basic tests

---
+++ data-structure/src/main/java/ar/com/old/nodes/Node.java
@@ -0,0 +1,10 @@
+package ar.com.old.nodes;
+
+public class Node {
+    public int value;
+    public Node next;
+
+    public Node(int value) {
+        this.value = value;
+    }
+}
---
+++ data-structure/src/test/java/ar/com/old/nodes/NodeTest.java
@@ -0,0 +1,20 @@
+package ar.com.old.nodes;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class NodeTest {
+
+    @Test
+    void shouldCreateNode() {
+        Node node = new Node(1);
+        assertEquals(1, node.value);
+        assertNotNull(node);
+    }
+
+    @Test
+    void shouldSetNullNextNode_whenNodeIsCreated() {
+        Node node = new Node(1);
+        assertNull(node.next);
+    }
+}
