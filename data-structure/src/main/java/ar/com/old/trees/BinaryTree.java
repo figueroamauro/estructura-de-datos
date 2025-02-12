7c706561f765ec749c3fc0ba076bcc7c93901275
mauro <figueroamauro.dev@gmail.com>
2025-02-11 21:01:17 -0300

    feat/test(BinaryTree): create BinaryTree class and implement add() method.

---
+++ data-structure/src/main/java/ar/com/old/trees/BinaryTree.java
@@ -0,0 +1,44 @@
+package ar.com.old.trees;
+
+import ar.com.old.nodes.TreeNode;
+
+public class BinaryTree {
+    private TreeNode root;
+
+    public BinaryTree() {
+        this.root = null;
+    }
+
+    //  4  2  5 7
+    public void add(int data) {
+        if (root == null) {
+            root = new TreeNode(data);
+            return;
+        }
+        TreeNode current = root;
+        while (true) {
+            if (data < current.data) {
+
+                if (current.left == null) {
+                    current.left = new TreeNode(data);
+                    return;
+                } else {
+                    current = current.left;
+                }
+            } else {
+                if (current.right == null) {
+                    current.right = new TreeNode(data);
+                    return;
+                } else {
+                    current = current.right;
+                }
+            }
+        }
+
+    }
+
+    public TreeNode getRoot() {
+        return root;
+    }
+
+}
---
+++ data-structure/src/test/java/ar/com/old/trees/BinaryTreeTest.java
@@ -0,0 +1,42 @@
+package ar.com.old.trees;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BinaryTreeTest {
+
+    @Test
+    void shouldCreateTree() {
+        BinaryTree tree = new BinaryTree();
+        assertNotNull(tree);
+    }
+
+    @Test
+    void shouldReturnRoot() {
+        BinaryTree tree = new BinaryTree();
+        assertNull(tree.getRoot());
+    }
+
+    @Test
+    void shouldAddRootNode_whenIsEmpty() {
+        BinaryTree tree = new BinaryTree();
+        tree.add(5);
+        tree.add(7);
+        assertEquals(5, tree.getRoot().data);
+    }
+
+    @Test
+    void shouldAddInCorrectPosition() {
+        BinaryTree tree = new BinaryTree();
+        tree.add(4);
+        tree.add(2);
+        tree.add(5);
+        tree.add(7);
+        assertEquals(4, tree.getRoot().data);
+        assertEquals(2, tree.getRoot().left.data);
+        assertEquals(5, tree.getRoot().right.data);
+        assertEquals(7, tree.getRoot().right.right.data);
+    }
+
+}
